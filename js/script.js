35fc8bf01013546691da428deb95031d126800fd
vvsafwan <124038973+vvsafwan@users.noreply.github.com>
2023-02-08 11:08:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function show(){
+    document.getElementById("btn-click").style.display="block";
+    document.getElementById("btn-click1").style.display="none";
+    document.getElementById("btn-click2").style.display="none";
+    document.getElementById("btn-click3").style.display="none";
+}
+function show1(){
+    document.getElementById("btn-click").style.display="none";
+    document.getElementById("btn-click1").style.display="block";
+    document.getElementById("btn-click2").style.display="none";
+    document.getElementById("btn-click3").style.display="none";
+}
+function show2(){
+    document.getElementById("btn-click").style.display="none";
+    document.getElementById("btn-click1").style.display="none";
+    document.getElementById("btn-click2").style.display="block";
+    document.getElementById("btn-click3").style.display="none";
+}
+function show3(){
+    document.getElementById("btn-click").style.display="none";
+    document.getElementById("btn-click1").style.display="none";
+    document.getElementById("btn-click2").style.display="none";
+    document.getElementById("btn-click3").style.display="block";
+}
+function showAlert(){
+    alert("this website is under maintainance")
+}
